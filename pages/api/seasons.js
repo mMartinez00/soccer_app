a7422055d72e34533604d45c7f3fa5605ce3fbc2
mMartinez00 <martinez.mauricio.0093@gmail.com>
2023-08-20 21:48:44 -0400

    Created API routes and dynamic routes that fetch and return data from the football API. They are server-side only bundles and won't increase client-side bundle size

---
+++ pages/api/seasons.js
@@ -0,0 +1,22 @@
+const fetchSeasons = async () => {
+  const response = await fetch(
+    "https://api-football-v1.p.rapidapi.com/v3/players/seasons",
+    {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": process.env.API_KEY,
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
+};
+
+export default async function handler(req, res) {
+  const response = await fetchSeasons();
+
+  res.status(200).json(response);
+}
