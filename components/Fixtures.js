d0a7ede3ff585989c954c6aa2135b7883747e0ac
mMartinez00 <martinez.mauricio.0093@gmail.com>
2023-10-18 21:59:39 -0400

    The data passed to the Fixtures component via props was sorted in alphabetical order based on the country of the leagues to which these fixtures belong. The fixtures are then grouped in an object by the key of a unique combination of league country and league name. Values are arrays of fixtures belonging to those keys.

--- components/Fixtures.js
+++ components/Fixtures.js
@@ -2,12 +2,41 @@ import React from "react";
 import Competition from "./Competition";
 
 export default function Fixtures({ data }) {
+  const sortFixturesByCountryAlphabetically = data.sort((a, b) => {
+    const leagueA = a.league.country.toUpperCase();
+    const leagueB = b.league.country.toUpperCase();
+
+    return leagueA > leagueB ? 1 : leagueA < leagueB ? -1 : 0;
+  });
+
+  const groupFixturesByLeague = sortFixturesByCountryAlphabetically.reduce(
+    (a, b) => {
+      const key = `${b.league.country} - ${b.league.name}`;
+      if (!a[key]) {
+        a[key] = [];
+      }
+
+      a[key].push(b);
+
+      return a;
+    },
+    {}
+  );
+
+  const leagueAndMatchesArray = Object.entries(groupFixturesByLeague);
+
   return (
-    <>
-      {data &&
-        data.response.map((fixture) => {
-          return <Competition key={fixture.id} fixture={fixture} />;
+    <section className="Fixtures">
+      {leagueAndMatchesArray &&
+        leagueAndMatchesArray.map((fixture) => {
+          return (
+            <Competition
+              key={fixture[0]}
+              league={fixture[1][0].league}
+              matches={fixture[1]}
+            />
+          );
         })}
-    </>
+    </section>
   );
 }
