1cee3814a091a5b6bf9b98c106487b6ee69ab95f
mMartinez00 <martinez.mauricio.0093@gmail.com>
2024-09-09 23:30:18 -0400

    changed font to 'Robo Mono', monospace

--- components/standings/StandingsTable.js
+++ components/standings/StandingsTable.js
@@ -3,9 +3,10 @@ import TableHead from './TableHead';
 import TableBody from './TableBody';
 
 export default function StandingsTable({ table }) {
+    console.log(table);
     return (
         <div className="Table_Container">
-            <h2>{table[0].group}</h2>
+            <h2 className="Table_Group">{table[0].group}</h2>
             <table className={`Standings_Table ${table[0].group}`}>
                 <TableHead />
 
