7ed496469e1681509ddfcee5fad2ccf775a593d7
mMartinez00 <martinez.mauricio.0093@gmail.com>
2025-02-01 19:33:15 -0500

    feat: display live game count in Live button

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
                                 showLive ? ' Active' : ''
                             }`}
                         >
-                            Live
+                            Live ({liveData.response.length})
                         </Button>
                         <InputText
                             inputValue={inputValue}
